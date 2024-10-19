91322bec0f567fea8eb7f99df942668fac0780f6
Victor-Buendia <victorbuendia03@gmail.com>
2024-10-19 17:06:04 -0300

    🐛 Fix grant permission not working

--- postgres/scripts/constraints.sql
+++ postgres/scripts/constraints.sql
@@ -8,3 +8,6 @@ ALTER TABLE inscricoes ADD CONSTRAINT id_inscricao_pk PRIMARY KEY (id_inscricao)
 ALTER TABLE eventos ADD CONSTRAINT id_empresa__eventos_to_empresas_fk FOREIGN KEY (id_empresa) REFERENCES empresas(id_empresa);
 ALTER TABLE inscricoes ADD CONSTRAINT id_evento__inscricoes_to_eventos_fk FOREIGN KEY (id_evento) REFERENCES eventos(id_evento);
 ALTER TABLE inscricoes ADD CONSTRAINT id_pessoa__inscricoes_to_pessoas_fk FOREIGN KEY (id_pessoa) REFERENCES pessoas(id_pessoa);
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO consumer;
+-- REVOKE ALL PRIVILEGES ON TABLE pessoa FROM consumer;
